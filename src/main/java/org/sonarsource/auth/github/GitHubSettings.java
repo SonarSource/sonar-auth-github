19b87d16ff46e9eacde31c626ebbd30523d114bf
Simon Brandhof <simon.brandhof@sonarsource.com>
2016-02-17 13:49:08 +0100

    Move settings to category "GitHub > Authentication"
    In order to be consistent with the other sonar-github-plugin
     which analyzes pull requests.

--- src/main/java/org/sonarsource/auth/github/GitHubSettings.java
+++ src/main/java/org/sonarsource/auth/github/GitHubSettings.java
@@ -44,8 +44,8 @@ public class GitHubSettings {
   public static final String LOGIN_STRATEGY_PROVIDER_ID = "Same as GitHub login";
   public static final String LOGIN_STRATEGY_DEFAULT_VALUE = LOGIN_STRATEGY_UNIQUE;
 
-  public static final String CATEGORY = "security";
-  public static final String SUBCATEGORY = "GitHub";
+  public static final String CATEGORY = "GitHub";
+  public static final String SUBCATEGORY = "Authentication";
 
   private final Settings settings;
 
