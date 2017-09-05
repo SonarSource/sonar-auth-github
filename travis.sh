e58b8afff75d9238bdd7cae6f24591fe4ce81879
Julien Lancelot <julien.lancelot@sonarsource.com>
2017-09-05 09:26:49 +0200

    Fix installation of jdk8 in travis

--- travis.sh
+++ travis.sh
@@ -3,10 +3,11 @@
 set -euo pipefail
 
 function configureTravis {
-  mkdir ~/.local
+  mkdir -p ~/.local
   curl -sSL https://github.com/SonarSource/travis-utils/tarball/v36 | tar zx --strip-components 1 -C ~/.local
   source ~/.local/bin/install
 }
 configureTravis
+. installJDK8
 
 regular_mvn_build_deploy_analyze
