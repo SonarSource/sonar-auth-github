cd84d2130e6fac5be75ab1fe7af80b4d4aeaad09
Simon Brandhof <simon.brandhof@sonarsource.com>
2018-02-08 09:56:39 +0100

    Upgrade to travis-utils v46

--- travis.sh
+++ travis.sh
@@ -4,10 +4,16 @@ set -euo pipefail
 
 function configureTravis {
   mkdir -p ~/.local
-  curl -sSL https://github.com/SonarSource/travis-utils/tarball/v38 | tar zx --strip-components 1 -C ~/.local
+  curl -sSL https://github.com/SonarSource/travis-utils/tarball/v46 | tar zx --strip-components 1 -C ~/.local
   source ~/.local/bin/install
 }
 configureTravis
 . installJDK8
+
+# When a pull request is open on the branch, then the job related
+# to the branch does not need to be executed and should be canceled.
+# It does not book slaves for nothing.
+# @TravisCI please provide the feature natively, like at AppVeyor or CircleCI ;-)
+cancel_branch_build_with_pr || if [[ $? -eq 1 ]]; then exit 0; fi
 
 regular_mvn_build_deploy_analyze
