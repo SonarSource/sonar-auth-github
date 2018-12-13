153f7c7af7a264adb0fcbe5fee87bdd140a6a3a1
Julien Lancelot <julien.lancelot@sonarsource.com>
2018-12-13 11:50:53 +0100

    Allow deployment of PR artifacts to repox

--- travis.sh
+++ travis.sh
@@ -15,6 +15,7 @@ configureTravis
 # @TravisCI please provide the feature natively, like at AppVeyor or CircleCI ;-)
 cancel_branch_build_with_pr || if [[ $? -eq 1 ]]; then exit 0; fi
 
+export DEPLOY_PULL_REQUEST=true
 regular_mvn_build_deploy_analyze
 
 ./check-license-compliance.sh
